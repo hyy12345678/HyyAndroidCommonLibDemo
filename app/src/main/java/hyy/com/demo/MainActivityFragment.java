a93b4981ad8dd62a134c4ed11efc07fb56875f58
kevin huang <hyy12345678@gmail.com>
2015-07-07 23:29:39 +0800

    又一套Android工具Lib
    刚开始做，只放了一个DropDownListView，后续会慢慢添加。作为个人工具的一个积累。

---
+++ app/src/main/java/hyy/com/demo/MainActivityFragment.java
@@ -0,0 +1,135 @@
+package hyy.com.demo;
+
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.hyy.android.common.view.DropDownListView;
+
+import java.text.SimpleDateFormat;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.LinkedList;
+
+import hyy.com.dropdownlistviewdemo.R;
+
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class MainActivityFragment extends Fragment {
+
+    private LinkedList<String> listItems = null;
+    private DropDownListView listView = null;
+
+
+    private ArrayAdapter<String> adapter;
+
+    private String[] mStrings = {"Aaaaaa", "Bbbbbb", "Cccccc", "Dddddd", "Eeeeee", "Ffffff",
+            "Gggggg", "Hhhhhh", "Iiiiii", "Jjjjjj", "Kkkkkk", "Llllll", "Mmmmmm", "Nnnnnn",};
+    public static final int MORE_DATA_MAX_COUNT = 3;
+    public int moreDataCount = 0;
+
+    public MainActivityFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.fragment_main, container, false);
+
+        //listView = (DropDownListView)view.findViewById(R.id.list_view);
+        listView = (DropDownListView) view.findViewById(R.id.list_view);
+
+        return view;
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        listView.setOnDropDownListener(new DropDownListView.OnDropDownListener() {
+            @Override
+            public void onDropDown() {
+                new GetDataTask(true).execute();
+            }
+        });
+
+        listView.setOnBottomListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                new GetDataTask(false).execute();
+            }
+        });
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                //ToastUtils.show(context, R.string.drop_down_tip);
+                Toast.makeText(getActivity(), "Pull down to refresh or scroll to bottom to load more", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        listItems = new LinkedList<String>();
+        listItems.addAll(Arrays.asList(mStrings));
+        adapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1, listItems);
+        listView.setAdapter(adapter);
+
+    }
+
+    private class GetDataTask extends AsyncTask<Void, Void, String[]> {
+
+        private boolean isDropDown;
+
+        public GetDataTask(boolean isDropDown) {
+            this.isDropDown = isDropDown;
+        }
+
+        @Override
+        protected String[] doInBackground(Void... params) {
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                ;
+            }
+            return mStrings;
+        }
+
+        @Override
+        protected void onPostExecute(String[] result) {
+
+            //Toast.makeText(getActivity(), "hello", Toast.LENGTH_SHORT).show();
+
+            if (isDropDown) {
+                listItems.addFirst("Added after drop down");
+                adapter.notifyDataSetChanged();
+
+                // should call onDropDownComplete function of DropDownListView at end of drop down complete.
+                SimpleDateFormat dateFormat = new SimpleDateFormat("MM-dd HH:mm:ss");
+                listView.onDropDownComplete(getString(R.string.update_at) + dateFormat.format(new Date()));
+            } else {
+                moreDataCount++;
+                listItems.add("Added after on bottom");
+                adapter.notifyDataSetChanged();
+
+                if (moreDataCount >= MORE_DATA_MAX_COUNT) {
+                    //listView.setHasMore(false);
+                }
+
+                // should call onBottomComplete function of DropDownListView at end of on bottom complete.
+                //listView.onBottomComplete();
+            }
+
+            super.onPostExecute(result);
+        }
+    }
+}
