ccc1b9ff563ce3be66c8c69b3c915ed79a26d575
hyymsi <hyy12345678@gmail.com>
2018-03-13 08:12:27 +0800

    daily commit

--- hyy-android-common-lib/src/main/java/com/hyy/android/common/view/regular/FormEditText.java
+++ hyy-android-common-lib/src/main/java/com/hyy/android/common/view/regular/FormEditText.java
@@ -2,6 +2,7 @@ package com.hyy.android.common.view.regular;
 
 import android.content.Context;
 import android.util.AttributeSet;
+import android.view.LayoutInflater;
 import android.widget.LinearLayout;
 
 /**
@@ -10,25 +11,52 @@ import android.widget.LinearLayout;
 
 public class FormEditText extends LinearLayout {
 
-
+    Context context;
+    private LayoutInflater inflater;
 
     public FormEditText(Context context) {
         super(context);
+        this.context = context;
+        init(null, 0);
     }
 
     public FormEditText(Context context, AttributeSet attrs) {
         super(context, attrs);
+        this.context = context;
+        init(attrs, 0);
     }
 
     public FormEditText(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+        this.context = context;
+        init(attrs, defStyleAttr);
     }
 
 
-    private void init(){
+    private void init(AttributeSet attrs, int defStyle){
 
+        //TODO Load attributes
+
+
+
+        initView();
+        initData();
+        initEvent();
     }
 
 
+
+    private void initView() {
+        inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+    }
+
+    private void initData() {
+
+    }
+
+    private void initEvent() {
+
+    }
 
 }
